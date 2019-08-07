ff14d8bbd59dc0a67ee3483b9c938c46a4ce43ff
xiaohui <2535418266@qq.com>
2019-08-07 18:30:59 +0800

    add ntp

--- RDA5981_SDK_MbedOS515_V1.3.7/RDA5981_SDK_MbedOS515_V1.3.7/TESTS/TARGET_RDA/airkiss/main.cpp
+++ RDA5981_SDK_MbedOS515_V1.3.7/RDA5981_SDK_MbedOS515_V1.3.7/TESTS/TARGET_RDA/airkiss/main.cpp
@@ -9,9 +9,12 @@
 #include "lwip_stack.h"
 
 #include <stdio.h>
+#include <time.h>
+
 #include <stdlib.h>
 #include <string.h>
 #include "cJSON.h"
+
 
 #include "lwip/api.h"
 #include "http.h"
@@ -37,7 +40,7 @@ void *wifi_main_msgQ;
   #error [NOT_SUPPORTED] test not supported
 #endif
 
-
+void tzset(void);
 WiFiStackInterface wifi;
 
 void wifi_status_thread(){
@@ -191,7 +194,7 @@ int scanNconnect_wifi(void){
 
 typedef struct Node
 {
-	int data;
+	unsigned char data;
 	struct Node *pNext;
 	struct Node *pPre;
 }NODE, *pNODE;
@@ -206,11 +209,11 @@ int IsEmptyDbLinkList(pNODE pHead);
 
 int GetLengthDbLinkList(pNODE pHead);
 
-int InsertEleDbLinkList(pNODE pHead, int pos, int data);
+int InsertEleDbLinkList(pNODE pHead, int pos, unsigned char data);
 
 int DeleteEleDbLinkList(pNODE pHead, int pos);
 
-int GetEleDbLinkList(pNODE pHead, int pos);
+unsigned char GetEleDbLinkList(pNODE pHead, int pos);
 
 void FreeMemory(pNODE *ppHead);
 
@@ -292,7 +295,7 @@ int GetLengthDbLinkList(pNODE pHead)
 	return length;
 }
 
-int InsertEleDbLinkList(pNODE pHead, int pos, int data)
+int InsertEleDbLinkList(pNODE pHead, int pos, unsigned char data)
 {
 	pNODE pt = 0, p_new = 0;
  
@@ -354,7 +357,7 @@ int DeleteEleDbLinkList(pNODE pHead, int pos)
 		return 0;
 }
 
-int GetEleDbLinkList(pNODE pHead, int pos)
+unsigned char GetEleDbLinkList(pNODE pHead, int pos)
 {
 	pNODE pt = 0;
  
@@ -514,7 +517,7 @@ void analyze_json(unsigned char *str){
 		
 		printf("test:%s\r\n",&tmp_hdr[1]);
 		printf("test t:%s\r\n",&((char **)tmp_hdr)[0]);
-		http_response_t* p_resp = http_request_w_body("http://wy.cmfspay.com/hardware/devicestatus", HTTP_REQ_POST, (char**)tmp_hdr, 2, Data->valuestring);
+		http_response_t* p_resp = http_request_w_body(Url->valuestring, HTTP_REQ_POST, (char**)tmp_hdr, 2, Data->valuestring);
 		printf("\r\nRESPONSE FROM :\n%s\n", p_resp->contents);
 		
 	}else if(strcmp(Type->valuestring,"socket")){
@@ -528,6 +531,7 @@ void analyze_json(unsigned char *str){
 		cJSON *Data = cJSON_GetObjectItem(root,"Data");
 		printf("Data:%s\n",Data->valuestring);
 		
+		//socket tcp? short? or long?
 		
 		
 	}else if(strcmp(Type->valuestring,"at")){
@@ -544,10 +548,94 @@ void analyze_json(unsigned char *str){
 	cJSON_Delete(root);
 }
 
-unsigned char json_buf[1024];
+  typedef struct
+  {
+
+    uint8_t li_vn_mode;      // Eight bits. li, vn, and mode.
+                             // li.   Two bits.   Leap indicator.
+                             // vn.   Three bits. Version number of the protocol.
+                             // mode. Three bits. Client will pick mode 3 for client.
+
+    uint8_t stratum;         // Eight bits. Stratum level of the local clock.
+    uint8_t poll;            // Eight bits. Maximum interval between successive messages.
+    uint8_t precision;       // Eight bits. Precision of the local clock.
+
+    uint32_t rootDelay;      // 32 bits. Total round trip delay time.
+    uint32_t rootDispersion; // 32 bits. Max error aloud from primary clock source.
+    uint32_t refId;          // 32 bits. Reference clock identifier.
+
+    uint32_t refTm_s;        // 32 bits. Reference time-stamp seconds.
+    uint32_t refTm_f;        // 32 bits. Reference time-stamp fraction of a second.
+
+    uint32_t origTm_s;       // 32 bits. Originate time-stamp seconds.
+    uint32_t origTm_f;       // 32 bits. Originate time-stamp fraction of a second.
+
+    uint32_t rxTm_s;         // 32 bits. Received time-stamp seconds.
+    uint32_t rxTm_f;         // 32 bits. Received time-stamp fraction of a second.
+
+    uint32_t txTm_s;         // 32 bits and the most important field the client cares about. Transmit time-stamp seconds.
+    uint32_t txTm_f;         // 32 bits. Transmit time-stamp fraction of a second.
+
+  } ntp_packet;              // Total: 384 bits or 48 bytes.
+#define NTP_TIMESTAMP_DELTA 2208988800ull
+UDPSocket udpsocket;
+char* host_name = "time.windows.com";
+void test_ntp(void){
+	long rLen=0;
+	
+	// Create and zero out the packet. All 48 bytes worth.
+	ntp_packet packet = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	memset( &packet, 0, sizeof( ntp_packet ) );
+	// Set the first byte's bits to 00,011,011 for li = 0, vn = 3, and mode = 3. The rest will be left set to zero.
+	*( ( char * ) &packet + 0 ) = 0x1b; // Represents 27 in base 10 or 00011011 in base 2.
+	
+	ip_addr_t server;
+	int ret = netconn_gethostbyname(host_name, &server);
+	if(ret != 0){
+		printf("dns error\r\n");
+	}
+
+	SocketAddress addr(inet_ntoa(server), 123);
+	if (!addr) {
+		printf("address error\r\n");
+		return;
+	}
+
+	udpsocket.open(&wifi);
+	udpsocket.bind(0);
+
+	rLen = udpsocket.sendto(inet_ntoa(server), 123, ( char* ) &packet, sizeof( ntp_packet ));
+
+	printf("udp recv test begin \r\n");
+	rLen = udpsocket.recvfrom(&addr,( void* ) &packet, sizeof( ntp_packet ));
+	//printf("recv %d Bytes \n", rLen);
+	if (rLen <= 0) {
+		printf("ERROR: UDP recv error, len is %d £¡\r\n", rLen);
+		return;
+	}
+	
+	//struct _reent reent;
+	//_putenv_r(&reent,"TZ=PST8PDT");
+	//setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 3);
+	//_tzset_r(&reent);
+	
+	packet.txTm_s = ntohl( packet.txTm_s ); // Time-stamp seconds.
+	packet.txTm_f = ntohl( packet.txTm_f ); // Time-stamp fraction of a second.
+	time_t txTm = ( time_t ) ( packet.txTm_s - NTP_TIMESTAMP_DELTA );
+	struct tm *info;
+	char buffer[80];
+	info = localtime(&txTm);
+	info->tm_hour += 8;
+	strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", info);
+	printf("time:%s\r\n",buffer);
+	printf("\tNTP timestamp is %s", ctime(&txTm));
+	printf("udp recv test end \r\n");
+}
+
+unsigned char json_buf[100];
 void deal_searial_input(void){
-	int tmp_pkg_len;
-	int tmp_pkg_len_i;
+	unsigned int tmp_pkg_len;
+	unsigned int tmp_pkg_len_i;
 	int tmp_pos = 1;
 	int tmp_Dat_pos;
 	unsigned char tmp_check;
@@ -556,8 +644,10 @@ void deal_searial_input(void){
 		tmp_pos = 1;
 		tmp_check = 0;
 		//wait hdr
+		printf("wait hdr\r\n");
 		while(1){
 			while(tmp_pos > GetLengthDbLinkList(head));
+			printf("read_fifo:%c[%x,%d]\r\n",GetEleDbLinkList(head, tmp_pos),GetEleDbLinkList(head, tmp_pos),GetEleDbLinkList(head, tmp_pos));
 			if(GetEleDbLinkList(head, tmp_pos) == '<'){
 				DeleteEleDbLinkList(head,tmp_pos);
 				break;
@@ -565,31 +655,39 @@ void deal_searial_input(void){
 			DeleteEleDbLinkList(head,tmp_pos);
 		}
 		//wait pkg len
-		while(tmp_pos > GetLengthDbLinkList(head));
+		printf("wait pkg len\r\n");
 		tmp_pos++;
+		while(GetLengthDbLinkList(head) < tmp_pos){printf("wait pkg len:%d\r\n",GetLengthDbLinkList(head));}
+		tmp_pkg_len = GetEleDbLinkList(head, tmp_pos);
+		tmp_check += GetEleDbLinkList(head, tmp_pos);
+		printf("wait pkg len:%d\r\n",GetEleDbLinkList(head, tmp_pos));
+		
+		tmp_pos++;
+		while(GetLengthDbLinkList(head) < tmp_pos);
+		tmp_pkg_len = tmp_pkg_len << 8;
 		tmp_pkg_len += GetEleDbLinkList(head, tmp_pos);
 		tmp_check += GetEleDbLinkList(head, tmp_pos);
-		while(tmp_pos > GetLengthDbLinkList(head));
-		tmp_pos++;
-		tmp_pkg_len += GetEleDbLinkList(head, tmp_pos);
-		tmp_check += GetEleDbLinkList(head, tmp_pos);
+		printf("wait pkg len:%d\r\n",GetEleDbLinkList(head, tmp_pos));
 		
 		tmp_pkg_len_i = tmp_pkg_len;
 		tmp_Dat_pos = tmp_pos + 1;
 		//rev data
+		printf("rev data tmp_pkg_len_i:%d\r\n",tmp_pkg_len_i);
 		while(tmp_pkg_len_i--){
-			while(tmp_pos > GetLengthDbLinkList(head));
 			tmp_pos++;
+			while(GetLengthDbLinkList(head) < tmp_pos);
 			tmp_check += GetEleDbLinkList(head, tmp_pos);
+			printf("rev data[%d]=%c\r\n",tmp_pos,GetEleDbLinkList(head, tmp_pos));
 		}
 		//rev crc
+		printf("rev crc : %x\r\n",tmp_check);
 		unsigned char check;
-		while(tmp_pos > GetLengthDbLinkList(head));
 		tmp_pos++;
+		while(GetLengthDbLinkList(head) < tmp_pos);
 		check = GetEleDbLinkList(head, tmp_pos);
 		
 		//check crc
-		
+		printf("check crc check:%d,tmp_check:%d\r\n",check,tmp_check);
 		if(check == tmp_check){
 			//rev data
 			memset(json_buf,0,sizeof(json_buf));
@@ -608,6 +706,7 @@ void deal_searial_input(void){
 			analyze_json(json_buf);
 		}else{
 			//crc err
+			printf("crc err check:%d,tmp_check:%d\r\n",check,tmp_check);
 			tmp_pos = 1;
 			break;
 		}
@@ -648,9 +747,13 @@ int main() {
 	printf("================================start http post test===================================\r\n");
 	http_post();
 	printf("================================            end        ===================================\r\n");
+	
+	printf("================================start ntp test===================================\r\n");
+	test_ntp();
+	printf("================================            end        ===================================\r\n");
 
 	printf("================================wifi.disconnect() test===================================\r\n");
-	wifi.disconnect();
+	//wifi.disconnect();
 	printf("================================            end        ===================================\r\n");
 
 	printf("************************************** start ************************************************\r\n");
@@ -673,8 +776,11 @@ int main() {
 	
 	printf("************************************************          end        ************************************************\r\n");
 
+	test_serial();
+
     while (true) {
 		//loop
+		
         
     }
 }
